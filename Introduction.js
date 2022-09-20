d81cd81dd6bb4ffd74d7c1ec7252ac9ad0fb8995
karantomar12697 <karansinghtomar12697@gmail.com>
2022-09-20 21:50:29 +0530

    Introduction

--- Introduction.js
+++ Introduction.js
@@ -4,11 +4,3 @@ let father_name = "Rakesh";
 console.log(father_name);
 let mother_name = "Gaytri";
 console.log(mother_name);
-
-
-
-
-let name1= "Masai School";
-let name2 = "A Transformation in Education";
-console.log(name1);
-console.log(name2);
